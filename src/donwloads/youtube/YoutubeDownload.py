cf9a9c38429962effab4f45c8a042115c31fae1d
matheus.barbosa <matheus.barbosa@perinity.com>
2023-08-25 22:01:34 -0300

    Add function download audio

--- src/donwloads/youtube/YoutubeDownload.py
+++ src/donwloads/youtube/YoutubeDownload.py
@@ -1,4 +1,5 @@
 from pytube import YouTube
+import os
 
 def download_video(url, resolution, folder=None):
     youtubeObject = YouTube(url)
@@ -9,3 +10,19 @@ def download_video(url, resolution, folder=None):
         print("Download is completed successfully")
     except Exception as e:
         print("An error has occurred", e)
+
+def download_audio(url, folder=None):
+    youtube = YouTube(url)
+    video = youtube.streams.filter(only_audio=True).first()
+
+    if folder is None:
+        folder = '.'
+
+    try: 
+        out_file = video.download(output_path=folder)
+        base, ext = os.path.splitext(out_file)
+        new_file = base + '.mp3'
+        os.rename(out_file, new_file)
+        print(youtube.title + " has been successfully downloaded.")
+    except Exception as e:
+        print("An error has occured", e)
